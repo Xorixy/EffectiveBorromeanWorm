48dd8e88779519f667f931efc3c7ad297f911237
igorimoshuk <iltimoshuk@gmail.com>
2024-08-08 15:31:08 +0200

    sim_params for first sim saved

--- sim_params.py
+++ sim_params.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-sim_name = 'Tetralith_test_2'
+sim_name = 'large_test'
 set_name = 'settings'
 
 
-single_weight_arr = np.linspace(0.4, 0.6, 2)
+single_weight_arr = np.linspace(0.46, 0.48, 5)
 counter_weight_arr = np.linspace(0.4, 0.5, 2)
-size_arr = np.linspace(40, 50, 2)
+size_arr = np.logspace(np.log10(8), np.log10(100), 10, dtype=np.int32)
+
+#(single_weight_t, 
+# counter_weight_t, 
+# size_t) = np.meshgrid(single_weight_arr, 
+#                       counter_weight_arr, 
+#                       size_arr)
 
 (single_weight_t, 
- counter_weight_t, 
  size_t) = np.meshgrid(single_weight_arr, 
-                       counter_weight_arr, 
                        size_arr)
+
+counter_weight_t = single_weight_t
 
 size = size_t.flatten()
 single_weight = single_weight_t.flatten()
@@ -22,12 +28,12 @@ size_x = size
 size_y = size
 
 mem_per_cpu = '500'
-time_limit = '00:10:00'
-devel = True
+time_limit = '01:30:00'
+devel = False
 
 n_samples = 5
-n_steps = 100000000 * np.ones_like(size)
-n_therm = 100000000 * np.ones_like(size)
+n_steps = 10 ** 10 * np.ones_like(size)
+n_therm = 10 ** 10 * np.ones_like(size)
 
 #size_x = 5
 #size_y = 5
