86ce2ef2e4aece1e000a6c17a81dc1e62f82ef9d
Xorixy <soricib@gmail.com>
2025-06-19 20:05:17 +0200

    Some work on launching batches for the bisection method

--- find_constant_curve/bisection.py
+++ find_constant_curve/bisection.py
@@ -6,6 +6,7 @@ import argparse
 import json
 import shutil
 
+
 def estimate_run_time(n_steps, n_therm):
     return int(2*(n_steps + n_therm)/(10 ** 7)) + 10
 def start_bisection():
@@ -27,8 +28,8 @@ def start_bisection():
     n_sim = p["n_sim"]
     counter_chi_factor = p["counter_chi_factor"]
     exec = p["exec_loc"]
-    launch_array(sim_folder, size, P, 0, n_steps, n_therm, counter_chi_factor, n_sim, exec)
-
+    #launch_array(sim_folder, size, P, 0, n_steps, n_therm, counter_chi_factor, n_sim, exec)
+    print(get_sim_result(sim_folder, n_sim))
 def bisection_step():
     print("Bisection step")
 
@@ -63,6 +64,36 @@ def create_settings_file(settings_loc, size, P, chi, n_steps, n_therm, counter_c
         f["settings/save/annulus_size"] = np.float64(0.5)
         f["settings/save/save_interval"] = np.int32(1)
 
+def get_sim_result(outfile, n_sims):
+    part_f = np.zeros(n_sims)
+    windings_diff_s_x = np.zeros(n_sims)
+    windings_diff_s_y = np.zeros(n_sims)
+    windings_sum_s_x = np.zeros(n_sims)
+    windings_sum_s_y = np.zeros(n_sims)
+    for i in range(n_sims):
+        file_path = outfile + "_" + str(i + 1) + ".h5"
+        with h5.File(file_path, "r") as sim_file:
+            base = np.array(sim_file['/constants/base_minus_one'], dtype=np.uint64).astype(dtype=object) + 1
+            part_f[i] = np.array(sim_file['/data/partition_function'], dtype=np.int64)
+            windings_diff_s_x[i] = (np.array(sim_file['/data/windings_diff_squared_x/big'], dtype=np.uint64) * base +
+                                    np.array(sim_file['/data/windings_diff_squared_x/small'], dtype=np.uint64))
+            windings_sum_s_x[i] = (np.array(sim_file['/data/windings_sum_squared_x/big'], dtype=np.uint64) * base +
+                                   np.array(sim_file['/data/windings_sum_squared_x/small'], dtype=np.uint64))
+            windings_diff_s_y[i] = (np.array(sim_file['/data/windings_diff_squared_y/big'], dtype=np.uint64) * base +
+                                    np.array(sim_file['/data/windings_diff_squared_y/small'], dtype=np.uint64))
+            windings_sum_s_y[i] = (np.array(sim_file['/data/windings_sum_squared_y/big'], dtype=np.uint64) * base +
+                                   np.array(sim_file['/data/windings_sum_squared_y/big'], dtype=np.uint64))
+    windings_diff_x = windings_diff_s_x / part_f
+    windings_sum_x = windings_sum_s_x / part_f
+    windings_diff_y = windings_diff_s_y / part_f
+    windings_sum_y = windings_sum_s_y / part_f
+    windings_diff = np.append(windings_diff_x, windings_diff_y)
+    windings_sum = np.append(windings_sum_x, windings_sum_y)
+    diff_mean = np.mean(windings_diff)
+    sum_mean = np.mean(windings_sum)
+    diff_var = np.var(windings_diff, ddof=1) / n_sims
+    sum_var = np.var(windings_sum, ddof=1) / n_sims
+    return diff_mean, sum_mean, diff_var, sum_var
 def get_chi_list(params):
     try:
         chis = np.array(params["chis"])
