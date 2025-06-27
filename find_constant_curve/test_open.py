8cbc965a8830b75ba44a8b1fe072b8ee2b0e196d
Alex <soricib@gmail.com>
2025-06-27 14:13:18 +0200

    Testing multiple sclurm scripts accessing the same file

--- find_constant_curve/test_open.py
+++ find_constant_curve/test_open.py
@@ -5,14 +5,21 @@ import h5py as h5
 max_tries = 100
 success = False
 
+
 while not success and max_tries > 0:
     try:
+        print("Trying to open file...")
         f = h5.File("res.h5", "a")
+        print("File opened successfully.")
         if len(f.keys()) == 0:
+            print("No data found, creating data = 1")
             f.create_dataset("data", data=1)
         else:
             data = f["data"][()]
             del f["data"]
+            print("Data found = ", str(data))
+            print("Incrementing...")
             f.create_dataset("data", data=(data + 1))
     except:
+        print("Failed to open file. Sleeping for 4s...")
         time.sleep(4)
