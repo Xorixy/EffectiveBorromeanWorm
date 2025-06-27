313a20f4660d97f1940d8d8c2aaaaac9e5a4ccf0
Alex <soricib@gmail.com>
2025-06-27 15:01:13 +0200

    Made bisection run multiple chis in parallell

--- find_constant_curve/bisection.py
+++ find_constant_curve/bisection.py
@@ -46,7 +46,7 @@ def start_bisection():
     res = try_load_h5(sim_folder + "/result.h5", "x")
     res.create_dataset("sym/P", data=P)
     sym_id = launch_array(sim_folder, size, P, 0, n_steps, n_therm, counter_chi_factor, n_sim, exec_loc, 1, True)
-    launch_bisection_step(sym_id, sim_folder, -1)
+    launch_bisection_step(sym_id, sim_folder, -1, 1)
     for i in range(len(chis)):
         start_new_chi_step(p, i)
 def bisection_step():
