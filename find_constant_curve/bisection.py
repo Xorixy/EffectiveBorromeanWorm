20e42d988b11687366af72d7a010148bb0849566
Alex <soricib@gmail.com>
2025-07-01 09:57:50 +0200

    Saved size to res

--- find_constant_curve/bisection.py
+++ find_constant_curve/bisection.py
@@ -50,6 +50,7 @@ def start_bisection():
     print(f"Estimated runtime : {estimate_run_time(n_steps, n_therm)}")
     res = try_load_h5(sim_folder + "/result.h5", "x")
     res.create_dataset("sym/P", data=P_sym)
+    res.create_dataset("sym/size", data=size)
     sym_id = launch_array(sim_folder + "/sim/sym", size, P_sym, 0, n_steps, n_therm, counter_chi_factor, n_sim, exec_loc, 1, True)
     launch_sym_step(sym_id, sim_folder)
 
